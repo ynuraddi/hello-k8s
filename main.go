1fda527812d389bdc38e7cb42e7b0690e7067953
ynuraddi <definston@gmail.com>
2025-03-05 23:58:43 +0500

    theme2: logs

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello K8s!"))
+		log.Println("Hello K8s!")
 	})
 
 	http.ListenAndServe(":8080", nil)
